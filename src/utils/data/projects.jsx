d2db0d2e8dc6d8b8a7bcf355fa551be925747388
ashokrawal <ashokrawaldz@gmail.com>
2025-06-25 01:29:59 +0100

    added project

--- src/utils/data/projects.jsx
+++ src/utils/data/projects.jsx
@@ -7,7 +7,24 @@ export const projects = {
         "Crocog is an online fresh vegetables store, which delivers from farm to your door steps",
       year: "2023",
       link: "https://github.com/Ashokrawal/cropcog",
-      tech: ["Next.js", "MongoDb", "Redux", "TailwindCSS", "Sass", "Prisma"],
+      tech: [
+        "Next.js",
+        "MongoDb",
+        "Redux",
+        "TailwindCSS",
+        "Sass",
+        "Prisma",
+        "Vercel",
+      ],
+    },
+    {
+      title: "WeatherApp",
+      image: "/weather.png",
+      description:
+        "A modern, responsive weather application built with Next.js and TypeScript that allows users to search for current weather conditions worldwide",
+      year: "2025",
+      link: "https://weathernow-chip.vercel.app/",
+      tech: ["Next.js", "TailwindCSS", "Vercel"],
     },
   ],
 };
