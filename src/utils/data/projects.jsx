9e0e4fa0ad6642fe0a4f04fb2ee9e6e3a3b178bf
ashokrawal <ashokrawaldz@gmail.com>
2025-06-27 09:14:18 +0100

    added new user authentication project

--- src/utils/data/projects.jsx
+++ src/utils/data/projects.jsx
@@ -18,13 +18,31 @@ export const projects = {
       ],
     },
     {
+      title: "user authentication app",
+      image: "/public/user.png",
+      description:
+        "User authentication process is done by verifying that a user is who they claim to be before granting them access to a system or resource. It's a crucial security measure that protects sensitive data and systems from unauthorized access.",
+      year: "2025",
+      link: "https://github.com/Ashokrawal/User-authentication",
+      tech: [
+        "Next.js",
+        "MongoDb",
+        "Redux",
+        "TailwindCSS",
+        "Mongoose",
+        "Nodemailer",
+        "JWT Auth",
+        "Vercel",
+      ],
+    },
+    {
       title: "WeatherApp",
       image: "/weather.png",
       description:
         "A modern, responsive weather application built with Next.js and TypeScript that allows users to search for current weather conditions worldwide",
       year: "2025",
       link: "https://weathernow-chip.vercel.app/",
-      tech: ["Next.js", "TailwindCSS", "Vercel"],
+      tech: ["Next.js", "TailwindCSS", "Vercel", "WeatherAPI"],
     },
   ],
 };
