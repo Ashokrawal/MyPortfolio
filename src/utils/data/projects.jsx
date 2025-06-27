cb2d590014a6b3be46a10a43a665bf46ca46590f
ashokrawal <ashokrawaldz@gmail.com>
2025-06-27 09:19:21 +0100

    added new photo

--- src/utils/data/projects.jsx
+++ src/utils/data/projects.jsx
@@ -1,25 +1,8 @@
 export const projects = {
   frontend: [
     {
-      title: "CropCog",
-      image: "/cropcog_project.png",
-      description:
-        "Crocog is an online fresh vegetables store, which delivers from farm to your door steps",
-      year: "2023",
-      link: "https://github.com/Ashokrawal/cropcog",
-      tech: [
-        "Next.js",
-        "MongoDb",
-        "Redux",
-        "TailwindCSS",
-        "Sass",
-        "Prisma",
-        "Vercel",
-      ],
-    },
-    {
-      title: "user authentication app",
-      image: "/public/user.png",
+      title: "user authentication",
+      image: "/user.png",
       description:
         "User authentication process is done by verifying that a user is who they claim to be before granting them access to a system or resource. It's a crucial security measure that protects sensitive data and systems from unauthorized access.",
       year: "2025",
